6a3961ca5fa75b1a63c9c66362e9d67456b87239
phantomXXI <haianhlol2001@gmail.com>
2021-11-29 11:11:47 +0700

    Add Message Error Login Admin

--- resources/views/admin_login.blade.php
+++ resources/views/admin_login.blade.php
@@ -2,18 +2,26 @@
 <head>
     <title>Trang quản lý Admin Web</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"/>
+    <script type="application/x-javascript"> addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        } </script>
     <!-- bootstrap-css -->
-    <link rel="stylesheet" href="{{ asset('public/backend/css/bootstrap.min.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public/backend/css/bootstrap.min.css') }}">
     <!-- //bootstrap-css -->
     <!-- Custom CSS -->
-    <link href="{{ asset('public/backend/css/style.css') }}" rel='stylesheet' type='text/css' />
+    <link href="{{ asset('public/backend/css/style.css') }}" rel='stylesheet' type='text/css'/>
     <link href="{{ asset('public/backend/css/style-responsive.css') }}" rel="stylesheet"/>
     <!-- font CSS -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link
+        href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
     <!-- font-awesome icons -->
     <link rel="stylesheet" href="{{ asset('public/backend/css/font.css') }}" type="text/css"/>
     <link href="{{ asset('public/backend/css/font-awesome.css') }}" rel="stylesheet">
@@ -27,7 +35,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <?php
         $message = Session::get('message');
         if ($message) {
-//            echo '<span class="text-alert" >';$message.'</span>' ;
+            echo '<span class="text-alert" style="color: red; font-size: 17px; min-width: 100%; text-align: center;font-weight: bold;">' . $message . '</span>';
             Session::get('message', null);
         }
         ?>
@@ -35,12 +43,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             {{ csrf_field() }}
             <input type="text" class="ggg" name="admin_email" placeholder="ĐIỀN E-MAIL" required="">
             <input type="password" class="ggg" name="admin_password" placeholder="ĐIỀN PASSWORD" required="">
-            <span><input type="checkbox" />Nhớ đăng nhập</span>
+            <span><input type="checkbox"/>Nhớ đăng nhập</span>
             <h6><a href="#">Quên mật khẩu?</a></h6>
             <div class="clearfix"></div>
             <input type="submit" value="Đăng nhập" name="login">
         </form>
-{{--        <p>Don't Have an Account ?<a href="registration.html">Create an account</a></p>--}}
+        {{--        <p>Don't Have an Account ?<a href="registration.html">Create an account</a></p>--}}
     </div>
 </div>
 <script src="{{ asset('public/backend/js/bootstrap.js') }}"></script>
