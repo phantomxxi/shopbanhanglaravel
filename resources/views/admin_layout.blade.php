927a2db1f7da490824b35c78e384a745ff4b3abb
phantomXXI <haianhlol2001@gmail.com>
2021-12-02 23:02:55 +0700

    Xu Ly Giao Dien Thương Hieu San Pham

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -106,6 +106,16 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                         </ul>
                     </li>
 
+                    <li class="sub-menu">
+                        <a href="javascript:;">
+                            <i class="fa fa-book"></i>
+                            <span>Thương hiệu sản phẩm</span>
+                        </a>
+                        <ul class="sub">
+                            <li><a href="{{ URL::to('/add-brand-product') }}">Thêm thương hiệu sản phẩm</a></li>
+                            <li><a href="{{ URL::to('/all-brand-product') }}">Liệt kê thương hiệu sản phẩm</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
             <!-- sidebar menu end-->
