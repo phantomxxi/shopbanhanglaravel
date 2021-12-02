ac2cf9d14f949c2285498a2791533dce9477357f
phantomXXI <haianhlol2001@gmail.com>
2021-12-02 21:11:00 +0700

    Xu Ly Logic Edit Danh Muc San Pham

--- app/Http/Controllers/CategoryProduct.php
+++ app/Http/Controllers/CategoryProduct.php
@@ -39,18 +39,42 @@ class CategoryProduct extends Controller
     }
 
 
-    public function unactive_category_product($category_id)
+    public function unactive_category_product($category_product_id)
     {
-        DB::table('tbl_category_product')->where('category_id', $category_id)->update(['category_status'=>1]);
+        DB::table('tbl_category_product')->where('category_id', $category_product_id)->update(['category_status'=>1]);
         Session::put('message', 'Không kích hoạt danh mục sản phẩm thành công');
         return Redirect::to('all-category-product');
     }
 
-    public function active_category_product($category_id)
+    public function active_category_product($category_product_id)
     {
-        DB::table('tbl_category_product')->where('category_id', $category_id)->update(['category_status'=>0]);
+        DB::table('tbl_category_product')->where('category_id', $category_product_id)->update(['category_status'=>0]);
         Session::put('message', 'Kích hoạt danh mục sản phẩm thành công');
         return Redirect::to('all-category-product');
     }
+
+    public function edit_category_product($category_product_id)
+    {
+        $edit_category_product = DB::table('tbl_category_product')->where('category_id', $category_product_id)->get();
+        $manager_category_product = view('admin.edit_category_product')->with('edit_category_product', $edit_category_product);
+        return view('admin_layout')->with('admin.edit_category_product', $manager_category_product);
+    }
+
+    public function delete_category_product()
+    {
+
+    }
+
+    public function update_category_product($category_product_id, Request $request)
+    {
+        $data = array();
+        $data['category_name'] = $request->category_product_name;
+        $data['category_desc'] = $request->category_product_desc;
+        DB::table('tbl_category_product')->where('category_id', $category_product_id)->update($data);
+        Session::put('message', 'Cập nhật danh mục sản phẩm thành công');
+        return Redirect::to('all-category-product');
+    }
+
+
 
 }
