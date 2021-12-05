6b94e73a4ccc909fc51c84cd5ff0abe833c03bcc
phantomXXI <haianhlol2001@gmail.com>
2021-12-05 10:26:13 +0700

    Xu Ly Giao Dien , Logic Edit Va Delete San Pham

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,12 +53,12 @@ class ProductController extends Controller
             $data['product_image'] = $new_image;
             DB::table('tbl_product')->insert($data);
             Session::put('message', 'Thêm sản phẩm thành công');
-            return Redirect::to('add-product');
+            return Redirect::to('all-product');
         }
         $data['product_image'] = '';
         DB::table('tbl_product')->insert($data);
         Session::put('message', 'Thêm sản phẩm thành công');
-        return Redirect::to('add-product');
+        return Redirect::to('all-product');
     }
 
 
@@ -81,24 +81,41 @@ class ProductController extends Controller
         $cate_product = DB::table('tbl_category_product')->orderBy('category_id', 'desc')->get();
         $brand_product = DB::table('tbl_brand')->orderBy('brand_id', 'desc')->get();
         $edit_product = DB::table('tbl_product')->where('product_id', $product_id)->get();
-        $manager_product = view('admin.edit_product')->with('edit_product', $edit_product);
+        $manager_product = view('admin.edit_product')->with('edit_product', $edit_product)->with('cate_product', $cate_product)->with('brand_product', $brand_product);
         return view('admin_layout')->with('admin.edit_product', $manager_product);
     }
 
-    public function update_brand_product($brand_product_id, Request $request)
+    public function update_product($product_id, Request $request)
     {
         $data = array();
-        $data['brand_name'] = $request->brand_product_name;
-        $data['brand_desc'] = $request->brand_product_desc;
-        DB::table('tbl_brand')->where('brand_id', $brand_product_id)->update($data);
-        Session::put('message', 'Cập nhật thương hiệu sản phẩm thành công');
-        return Redirect::to('all-brand-product');
+        $data['product_name'] = $request->product_name;
+        $data['product_price'] = $request->product_price;
+        $data['product_desc'] = $request->product_desc;
+        $data['product_content'] = $request->product_content;
+        $data['category_id'] = $request->product_cate;
+        $data['brand_id'] = $request->product_brand;
+        $data['product_status'] = $request->product_status;
+        $get_image = $request->file('product_image');
+        if ($get_image){
+            $get_name_image = $get_image->getClientOriginalName();
+            // explode de tach chuoi sau dau '.', current de lay chuoi dau tien
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image.rand(0, 99).'.'.$get_image->getClientOriginalExtension();
+            $get_image->move('public/uploads/product', $new_image);
+            $data['product_image'] = $new_image;
+            DB::table('tbl_product')->where('product_id', $product_id)->update($data);
+            Session::put('message', 'Cập nhật sản phẩm thành công');
+            return Redirect::to('all-product');
+        }
+        DB::table('tbl_product')->where('product_id', $product_id)->update($data);
+        Session::put('message', 'Cập nhật sản phẩm thành công');
+        return Redirect::to('all-product');
     }
 
-    public function delete_brand_product($brand_product_id)
+    public function delete_product($product_id)
     {
-        DB::table('tbl_brand')->where('brand_id', $brand_product_id)->delete();
-        Session::put('message', 'Xóa thương hiệu sản phẩm thành công');
-        return Redirect::to('all-brand-product');
+        DB::table('tbl_product')->where('product_id', $product_id)->delete();
+        Session::put('message', 'Xóa sản phẩm thành công');
+        return Redirect::to('all-product');
     }
 }
--- resources/views/admin/edit_product.blade.php
+++ resources/views/admin/edit_product.blade.php
@@ -16,33 +16,39 @@
                     }
                     ?>
                     <div class="position-center">
-                        <form role="form" action="{{ URL::to('/update-product') }}" method="post" enctype="multipart/form-data">
+                        @foreach($edit_product as $key => $pro)
+                        <form role="form" action="{{ URL::to('/update-product/'.$pro->product_id) }}" method="post" enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Tên sản phẩm</label>
-                                <input type="text" name="product_name" class="form-control" id="exampleInputEmail1" placeholder="Tên sản phẩm">
+                                <input type="text" name="product_name" class="form-control" id="exampleInputEmail1" value="{{$pro->product_name}}">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Giá sản phẩm</label>
-                                <input type="text" name="product_price" class="form-control" id="exampleInputEmail1" placeholder="Giá sản phẩm">
+                                <input type="text" name="product_price" class="form-control" id="exampleInputEmail1" value="{{$pro->product_price}}">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Hình ảnh sản phẩm</label>
                                 <input type="file" name="product_image" class="form-control" id="exampleInputEmail1">
+                                <img src="{{ URL::to('public/uploads/product/'.$pro->product_image) }}" alt="" height="100" width="100">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Mô tả sản phẩm</label>
-                                <textarea style="resize: none" rows="8" name="product_desc" class="form-control" id="exampleInputPassword1" placeholder="Mô tả sản phẩm"></textarea>
+                                <textarea style="resize: none" rows="8" name="product_desc" class="form-control" id="exampleInputPassword1" value="{{$pro->product_desc}}"></textarea>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Nội dung sản phẩm</label>
-                                <textarea style="resize: none" rows="8" name="product_content" class="form-control" id="exampleInputPassword1" placeholder="Nội dung sản phẩm"></textarea>
+                                <textarea style="resize: none" rows="8" name="product_content" class="form-control" id="exampleInputPassword1" value="{{$pro->product_content}}"></textarea>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Danh mục sản phẩm</label>
                                 <select name="product_cate" class="form-control input-sm m-bot15">
                                     @foreach($cate_product as $key => $cate)
+                                        @if($cate->category_id == $pro->category_id)
+                                        <option selected value="{{ $cate->category_id }}">{{ $cate->category_name }}</option>
+                                        @else
                                         <option value="{{ $cate->category_id }}">{{ $cate->category_name }}</option>
+                                        @endif
                                     @endforeach
                                 </select>
                             </div>
@@ -50,7 +56,11 @@
                                 <label for="exampleInputPassword1">Thương hiệu</label>
                                 <select name="product_brand" class="form-control input-sm m-bot15">
                                     @foreach($brand_product as $key => $brand)
+                                        @if($brand->brand_id == $pro->brand_id)
+                                        <option selected value="{{ $brand->brand_id }}">{{ $brand->brand_name }}</option>
+                                        @else
                                         <option value="{{ $brand->brand_id }}">{{ $brand->brand_name }}</option>
+                                        @endif
                                     @endforeach
                                 </select>
                             </div>
@@ -63,6 +73,7 @@
                             </div>
                             <button type="submit" name="add_product" class="btn btn-info">Cập nhật sản phẩm</button>
                         </form>
+                        @endforeach
                     </div>
 
                 </div>
